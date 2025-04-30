b4de413541bfba5a18e1d9a51c3860f37656f0ef
Boris Lito <boris12lit@gmail.com>
2025-04-30 20:06:47 +0300

    add JS0

---
+++ JS/JS0_Tips_N_Tricks.js
@@ -0,0 +1,37 @@
+const NeoString = 'Wake up, Neo...'
+console.log(
+    (NeoString.length)
+)
+console.log(
+    NeoString.toUpperCase()
+)
+
+const Str1 = '1234'
+console.log(
+    typeof(+Str1)
+)
+console.log(
+    Number(Str1)
+)
+console.log(
+    String(+Str1)
+)
+console.log(
+    Boolean(Str1)
+)
+
+console.log(
+    (44 / '22')
+)
+// -> 2
+
+console.log(
+    isNaN(45 / 'yep')
+)
+// -> true
+
+console.log(
+    'con' + 'cat'
+)
+// -> concat
+
