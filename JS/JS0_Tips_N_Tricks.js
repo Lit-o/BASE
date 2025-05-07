8c5910cb7077c0bf6c3db6e31311a62a121234bf
Boris Lito <boris12lit@gmail.com>
2025-05-07 17:15:39 +0300

    add FizzBuzz and ChessField

--- JS/JS0_Tips_N_Tricks.js
+++ JS/JS0_Tips_N_Tricks.js
@@ -35,3 +35,45 @@ console.log(
 )
 // -> concat
 
+// Fizz Buzz
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
+        console.log('Fizz')
+    } else if (i % 5 === 0) {
+        console.log('Buzz')
+    } else {
+        console.log(i)
+    }
+}
+
+// FizzBuzz
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('«FizzBuzz»')
+    } else {
+        console.log(i)
+    }
+}
+
+const ChessFieldVer1 = (size = 8) => {
+    let lineArr = []
+
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++ ){
+            let firstElem = i % 2 === 0 ? '#' : ' '
+            let secondElem = i % 2 === 0 ? ' ' : '#'
+
+            if (j % 2 === 0) {
+                lineArr.push(firstElem)
+            } else {
+                lineArr.push(secondElem)
+            }
+            if (j + 1 === size) {
+                lineArr.push('\n')
+            }
+        } 
+    }
+
+    console.log(lineArr.join(''))
+}
+ChessFieldVer1(12)
