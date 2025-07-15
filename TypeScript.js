1a10d05ca838765ba11c1271f7f04d09d007e582
Boris Lito <boris12lit@gmail.com>
2025-07-15 23:17:36 +0300

    update ts

---
+++ TypeScript.js
@@ -0,0 +1,20 @@
+// clean start
+// npm i typescript -D
+// npx tsc --init
+// local use
+// npx tsc ./TypeScript.ts   
+// npx ts-node TypeScript.ts
+// 7 primitives: number, string, boolean, null, undefined, BigInt, Symbol
+var a = 5;
+// a type not number, but 5
+var b = 15;
+var c = 'string';
+var d = true;
+var e = null;
+var f = undefined;
+var num = 5;
+// num self typed to number
+num = -5;
+console.log(num);
+num = 1.2;
+console.log(num);
